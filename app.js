0b6f812bed475bf66e4e522801549324569110d8
TheTulatula <tnidam@gmail.com>
2024-12-14 13:43:55 +0000

    JS
    back to beginning
    split updateCity to 2 functions in order to update the selected city time

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let currentIntervalId;
+
 function updateTime() {
   let jerusalemElement = document.querySelector("#jerusalem");
   if (jerusalemElement) {
@@ -38,10 +40,10 @@ function updateCity(event) {
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-
   let cityTime = moment().tz(cityTimeZone);
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityName = getCityName(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
+
   citiesElement.innerHTML = `<div class="city">
     <div>
       <h2>${cityName}</h2>
@@ -52,6 +54,30 @@ function updateCity(event) {
   )}</small></div>
   </div>
 `;
+
+  if (currentIntervalId) {
+    clearInterval(currentIntervalId);
+  }
+  updateDateTime(cityTimeZone);
+
+  currentIntervalId = setInterval(function () {
+    updateDateTime(cityTimeZone);
+  }, 1000);
+}
+
+function getCityName(cityTimeZone) {
+  return cityTimeZone.replace("_", " ").split("/")[1];
+}
+function updateDateTime(cityTimeZone) {
+  let cityTime = moment().tz(cityTimeZone);
+  let dateElement = document.querySelector(".city .date");
+  let timeElement = document.querySelector(".city .time");
+  if (dateElement && timeElement) {
+    dateElement.innerHTML = cityTime.format("MMMM Do YYYY");
+    timeElement.innerHTML = `${cityTime.format(
+      "HH:mm:ss"
+    )} <small>${cityTime.format("A")}</small>`;
+  }
 }
 
 updateTime();
